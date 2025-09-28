83051de14e86aeb6d6420ee0fa54309871083392
Ephraimnorbat <ephraimnorbat@gmail.com>
2025-09-28 17:12:26 +0300

    removed prisma files

--- server/routes/subcategory.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createSubcategory } = require("../controllers/subcategory");
-
-router.post("/", createSubcategory);
-
-module.exports = router;
